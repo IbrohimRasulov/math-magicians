b5118a12091c67f4dcde8922d287913ea9193e67
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-18 03:16:58 +0500

    Make code clean by removing import React

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,8 @@
-import React from 'react';
-
-const Quote = () => {
-  return (
-    <div className="quote">
-      <p>Dear Math, please grow up and solve your own problems. I’m tired of solving them for you. <span>— Anonymous</span></p>
-      <p>If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is. <span>— John von Neumann</span></p>
-    </div>
-  );
-}
+const Quote = () => (
+  <div className="quote">
+    <p>Dear Math, please grow up and solve your own problems. I’m tired of solving them for you. <span>— Anonymous</span></p>
+    <p>If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is. <span>— John von Neumann</span></p>
+  </div>
+);
 
 export default Quote;

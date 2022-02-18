d73e9d6d2c5ea4738b3c68cb0f5f401a4d75282d
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-19 00:17:59 +0500

    Test navbar

--- src/components/__test__/Navbar.test.js
+++ src/components/__test__/Navbar.test.js
@@ -1,7 +1,12 @@
 import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from '../Navbar';
 
 test('Render Navbar component', () => {
-  const tree = renderer.create(<Navbar />);
+  const tree = renderer.create(
+    <Router>
+      <Navbar />
+    </Router>,
+  );
   expect(tree).toMatchSnapshot();
 });

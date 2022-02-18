4900992239979f9b2d61ec8dc787be6a43bc76c5
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-18 05:43:13 +0500

    Style quotes component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,10 @@
 const Quote = () => (
   <div className="quote">
-    <p>Dear Math, please grow up and solve your own problems. I’m tired of solving them for you. <span>— Anonymous</span></p>
-    <p>If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is. <span>— John von Neumann</span></p>
+    <fieldset>
+      <legend>Quotes</legend>
+      <p>Dear Math, please grow up and solve your own problems. I’m tired of solving them for you. <span>— Anonymous</span></p>
+      <p>If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is. <span>— John von Neumann</span></p>
+    </fieldset>
   </div>
 );
 
